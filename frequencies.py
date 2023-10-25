90e9506d5cf2754c00def8dbdb7ff64ddd3fb9f4
kamiliaps <76583821+kamiliaps@users.noreply.github.com>
2023-10-25 01:24:17 +0100

    Extend frequencies.py to calculate frequency of item in a list

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in range(len(items)):
+        items[i] = str(items[i])
+
+    for item in items:
+        frequency = items.count(item)
+        item = str(item)
+        frequencies[item] = frequency
+        
     return frequencies
+
